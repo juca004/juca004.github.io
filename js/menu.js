7f3115b0bd678accc4786ed3307f1739b66cda43
jucaflogar <juca210784@gmail.com>
2024-04-01 20:26:04 -0600

    se desarrolla un contador de visitas para la pagina de flogarTi

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,13 @@
 console.log("hola mundo");
+
+let contador = document.getElementById('contador');//contador es igual a cero
+
+if (localStorage.pagecount)
+{
+    localStorage.pagecount=Number(localStorage.pagecount) +1;
+}
+else
+{
+    localStorage.pagecount=1;
+}
+contador.innerHTML = `<p>${localStorage.pagecount}</p>`;
